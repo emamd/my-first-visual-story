16326157178b5e8dc898dcbd39abd1547b5dc44a
Armand Emamdjomeh <armand.emamdjomeh@washpost.com>
2022-02-26 14:57:09 -0500

    working on map

--- scripts/map.js
+++ scripts/map.js
@@ -1,3 +1,15 @@
 import * as L from 'leaflet';
+import homicides from '../_data/harvard_park_homicides.json';
 
-console.log(L);
+const map = L.map('map');
+const satelliteLayer = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA'
+);
+satelliteLayer.addTo(map);
+map.setView([33.983265, -118.306799], 18);
+
+homicides.forEach((obj) => {
+  L.circleMarker([obj.latitude, obj.longitude])
+    .addTo(map)
+    .bindTooltip(obj.first_name + ' ' + obj.last_name, { permanent: true });
+});
