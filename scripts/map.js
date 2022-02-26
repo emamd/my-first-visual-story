d57b8f6a2aeaade3132c88e4489f26c032fd6032
Armand Emamdjomeh <armand.emamdjomeh@washpost.com>
2022-02-26 15:23:07 -0500

    finished up map section woo

--- scripts/map.js
+++ scripts/map.js
@@ -1,9 +1,15 @@
 import * as L from 'leaflet';
+import MiniMap from 'leaflet-minimap';
 import homicides from '../_data/harvard_park_homicides.json';
 
-const map = L.map('map');
+const map = L.map('map', {
+  scrollWheelZoom: false,
+});
 const satelliteLayer = L.tileLayer(
-  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA'
+  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA',
+  {
+    minZoom: 13,
+  }
 );
 satelliteLayer.addTo(map);
 map.setView([33.983265, -118.306799], 18);
@@ -13,3 +19,12 @@ homicides.forEach((obj) => {
     .addTo(map)
     .bindTooltip(obj.first_name + ' ' + obj.last_name, { permanent: true });
 });
+
+const satelliteLayer2 = L.tileLayer(
+  'https://api.mapbox.com/styles/v1/mapbox/satellite-streets-v9/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibGF0aW1lcyIsImEiOiJjanJmNjg4ZzYweGtvNDNxa2ZpZ2lma3Z4In0.g0lYVIEs9Y5QcUOhXactHA',
+  {
+    maxZoom: 8,
+  }
+);
+const miniMap = new MiniMap(satelliteLayer2);
+miniMap.addTo(map);
