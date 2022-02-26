3bee1a65ec69fd5df1a091655fcb49b657d7cb4e
Armand Emamdjomeh <armand.emamdjomeh@washpost.com>
2022-02-25 19:33:39 -0500

    fixed the index.html content

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,3 @@
 // Write your code!
 console.log('Hello bakers');
-
 import './map';
-
-// Grab some data
-const dataElement = document.getElementById('example-data');
-const data = JSON.parse(dataElement.textContent);
-console.log(data);
